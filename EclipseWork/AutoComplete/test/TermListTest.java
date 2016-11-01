8b3bd2cec34bc31a78be416e09b897c1033f1db3
David Larkin <davidlarkin96@gmail.com>
2016-11-01 16:50:24 +0000

    Added Test for TermList

--- EclipseWork/AutoComplete/test/TermListTest.java
+++ EclipseWork/AutoComplete/test/TermListTest.java
@@ -1,12 +1,30 @@
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TermListTest {
 
+	private Term term1;
+	private Term term2;
+	private ArrayList<Term> termsList;
+	private TermList terms = new TermList();
+	
+	@Before
+	public void SetUp() throws IOException
+	{
+		terms.readFile();
+		termsList = new ArrayList<Term>();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetTermList() 
+	{
+		terms.getTermList();
+		assertTrue(terms.getTermList().size() > 1);
 	}
 
 }
