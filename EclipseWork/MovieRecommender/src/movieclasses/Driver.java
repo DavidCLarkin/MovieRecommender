b04276ac308497fa143484eb283e0f35545dd339
David Larkin <davidlarkin96@gmail.com>
2016-11-14 18:23:32 +0000

    Reading data from Ratings and User files, and creating objects in ArrayList of each file

--- EclipseWork/MovieRecommender/src/movieclasses/Driver.java
+++ EclipseWork/MovieRecommender/src/movieclasses/Driver.java
@@ -1,5 +1,20 @@
 package movieclasses;
 
 public class Driver {
+	
+	public static UserList userList = new UserList();
+	public static RatingList ratingList = new RatingList();
+	
+	
+	public Driver()
+	{
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		UserList.readFile("D:/Programming/EclipseWork/MovieRecommender/data/users5.dat");
+		RatingList.readFile("D:/Programming/EclipseWork/MovieRecommender/data/ratings5.dat");
+		System.out.println(ratingList.getRatingList().get(2));
+	}
 
 }
--- EclipseWork/MovieRecommender/src/movieclasses/Rating.java
+++ EclipseWork/MovieRecommender/src/movieclasses/Rating.java
@@ -5,4 +5,40 @@ public class Rating {
 	private int userID;
 	private int movieID;
 	private int rating;
+	
+	public Rating(int userID, int movieID, int rating)
+	{
+		this.userID = userID;
+		this.movieID = movieID;
+		this.rating = rating;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public int getMovieID() {
+		return movieID;
+	}
+
+	public void setMovieID(int movieID) {
+		this.movieID = movieID;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	@Override
+	public String toString() {
+		return "Rating: userID = " + userID + ", movieID = " + movieID + ", rating = " + rating;
+	}
 }
